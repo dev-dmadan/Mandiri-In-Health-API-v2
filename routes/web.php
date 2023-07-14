af94551a4238493ed0a6e74e2ce25aa9fcd7ea03
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2023-07-14 13:38:10 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Models\Contact;
 use App\Models\TreeTest;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,34 @@ use Illuminate\Support\Facades\Schema;
 |
 */
 
-Route::get('/', function () {
-    $contact = Contact::all()->first();
-    echo Carbon::parse($contact->CreatedOn, 'UTC')
-        ->setTimezone('Asia/Jakarta')
-        ->format('d-m-Y');
+Route::middleware('secret_key_is_valid')->group( function () {
+
+    Route::group(['prefix' => 'dashboard'], function () {
+        // performanceNasional
+        Route::get('/performance-nasional', [DashboardController::class, 'performanceNasional']);
+        Route::get('/performance-nasional/{id}', [DashboardController::class, 'performanceNasional'])
+            ->whereUuid('id');
+        
+        // quotationDashboard
+        Route::get('/quotation-dashboard', [DashboardController::class, 'quotationDashboard']); 
+        
+        // pipelineNewBusinessDashboard
+        Route::get('/pipeline-new-business-dashboard', [DashboardController::class, 'pipelineNewBusinessDashboard']); 
+        
+        // pipelineRenewalDashboard
+        Route::get('/pipeline-renewal-dashboard', [DashboardController::class, 'pipelineRenewalDashboard']); 
+        
+        // pipelineNewBusinessDashboardKanal
+        Route::get('/pipeline-new-business-dashboard-kanal/{kanalDistribusiId}', [DashboardController::class, 'pipelineNewBusinessDashboardKanal'])
+            ->whereUuid('kanalDistribusiId');
+            
+        // pipelineNewBusinessDashboardKanal
+        Route::get('/pipeline-renewal-dashboard-kanal/{kanalDistribusiId}', [DashboardController::class, 'pipelineRenewalDashboardKanal'])
+            ->whereUuid('kanalDistribusiId');
+    });
+
+    Route::group(['prefix' => 'report'], function () {
+        
+    });
+
 });
